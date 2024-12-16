4efd9fde9511528a794c0d33398d2e6d0a961380
ilchan123 <dlfcks9@gmail.com>
2024-12-16 15:54:19 +0900

    글꼴 및 색상 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,214 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView  } from 'react-native';
+import Colors from '@/constants/Colors';
+import Typography from '@/constants/Typography';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const App = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+            <View style={styles.colorbox}>      
+              <Text style={styles.Orange000}>orange000</Text>
+              <Text style={styles.Orange100}>orange100</Text>
+              <Text style={styles.Orange200}>orange200</Text>
+              <Text style={styles.Orange300}>orange300</Text>    
+            </View>
+            <View style={styles.colorbox}>      
+              <Text style={styles.white}>white</Text>
+              <Text style={styles.gray100}>gray100</Text>
+              <Text style={styles.gray200}>gray200</Text>
+              <Text style={styles.gray300}>gray300</Text>  
+              <Text style={styles.gray400}>gray400</Text>  
+              <Text style={styles.gray500}>gray500</Text>    
+            </View>
+            <View style={styles.colorbox}>      
+              <Text style={styles.red}>red</Text>
+              <Text style={styles.blue}>blue</Text>
+              <Text style={styles.brown}>brown</Text>   
+            </View>
+            <View style={styles.typobox}>      
+              <Text style={styles.display_large}>display_large</Text>
+              <Text style={styles.display_medium}>display_medium</Text>
+              <Text style={styles.display_small}>display_small</Text>   
+            </View>
+            <View style={styles.typobox}>      
+              <Text style={styles.heading_large}>heading_large</Text>
+              <Text style={styles.heading_medium}>heading_medium</Text>
+              <Text style={styles.heading_small}>heading_small</Text>   
+              <Text style={styles.heading_small_bold}>heading_small_bold</Text> 
+            </View>
+            <View style={styles.typobox}>      
+              <Text style={styles.body_large}>body_large</Text>
+              <Text style={styles.body_large_bold}>body_large_bold</Text>   
+              <Text style={styles.body_medium}>body_medium</Text>
+            </View>
+            <View style={styles.typobox}>      
+              <Text style={styles.label_large}>label_large</Text>
+              <Text style={styles.label_small}>label_small</Text>
+              <Text style={styles.label_very_small}>label_very_small</Text>   
+            </View>
+        </View>
+    </ScrollView>
+
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.gray.gray100, // 배경색 Constants에서 가져오기
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  colorbox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.gray.gray200,
   },
+  typobox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.gray.gray200,
+  },
+  Orange000: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.orange.orange000, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  Orange100: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.orange.orange100, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  Orange200: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.orange.orange200, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  Orange300: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.orange.orange300, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  white: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.white, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  gray100: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray100, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  gray200: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray200, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  gray300: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray300, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  gray400: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray400, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  gray500: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  red: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.system.red, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  blue: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.system.blue, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  brown: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.system.brown, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  display_large: {
+    ...Typography.display.display_large,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  display_medium: {
+    ...Typography.display.display_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  display_small: {
+    ...Typography.display.display_small,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  heading_large: {
+    ...Typography.headig.heading_large,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  heading_medium: {
+    ...Typography.headig.heading_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  heading_small: {
+    ...Typography.headig.heading_small,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  heading_small_bold: {
+    ...Typography.headig.heading_small_bold,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  body_large: {
+    ...Typography.body.body_large,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  body_large_bold: {
+    ...Typography.body.body_large_bold,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  body_medium: {
+    ...Typography.body.body_medium,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  label_large: {
+    ...Typography.label.label_large,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  label_small: {
+    ...Typography.label.label_small,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+  label_very_small: {
+    ...Typography.label.label_very_small,   // Typography에서 폰트 사이즈 가져오기
+    color: Colors.gray.gray500, // 텍스트 색상 Constants에서 가져오기
+    fontWeight: 'bold',
+  },
+ 
 });
+
+export default App;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,34 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
-export const Colors = {
+const Colors = {
+  gray: {
+      gray500: '#212121',     // 진한 회색
+      gray400: '#616161',  
+      gray300: '#9F9F9F',  
+      gray200: '#E0E0E0',     
+      gray100: '#F5F5F5',      // 연한 회색
+      white: '#FFFFFF',        // 하얀색
+  },
+  orange: {
+      orange300: '#F57C00',   // 진한 주황색
+      orange200: '#FF9800',   // 프라이머리 색!!!(메인 색)
+      orange100: '#FFB74D',
+      orange000: '#FFCC80',   // 연한 주황색
+  },
+  system: {
+    red: '#FF5252',     // 빨간색
+    blue: '#448AFF',    // 파랑색
+    brown: '#8D6E63',   // 갈색
+  },
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    tint: '#2f95dc', 
+    background: '#ffffff',
+    text: '#000000',
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    tint: '#ffffff',
+    background: '#000000',
+    text: '#ffffff',
   },
-};
+}
+
+
+export default Colors;
