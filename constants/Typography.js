093360de654cc2f2a30cae93e83ba2425d99389f
ilchan123 <dlfcks9@gmail.com>
2024-12-16 22:48:08 +0900

    feat: 글꼴 및 색상 수정

---
+++ constants/Typography.js
@@ -0,0 +1,105 @@
+import Color from './Colors';
+
+const Typography = {
+    display: {
+        display_large: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 57,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        display_medium: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 45,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        display_small: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 36,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+    },
+    heading: {
+        heading_large: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 32,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        heading_medium: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 28,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        heading_small: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 24,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        heading_small_bold: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 24,
+            fontWeight: '700',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+    },
+    body: {
+        body_large: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 16,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        body_medium: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 14,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        body_large_bold: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 16,
+            fontWeight: '700',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+    },
+    label: {
+        label_large: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 14,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        label_small: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 11,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+        label_very_small: {
+            fontFamily: 'Roboto Mono',
+            fontSize: 8,
+            fontWeight: '500',
+            lineHeight: 150,
+            color: Color.gray.gray500,
+        },
+    },
+};
+
+export default Typography;
