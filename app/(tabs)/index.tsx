b272cb8e353858130fa2bf58922f9a556ec3ee7f
박치호 <qkrclgh2@gmail.com>
2024-12-16 17:27:34 +0900

    feat: 하단 네이게이션바 제작

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,57 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// 각 화면 가져오기
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import PaymentScreen from '../screens/PaymentScreen';
+import CartScreen from '../screens/CartScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
-export default function HomeScreen() {
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+          if (route.name === 'Home') {
+            iconName = 'fish';
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          } else if (route.name === 'Chat') {
+            iconName = 'chat';
+            return <MaterialCommunityIcons name="chat" size={size} color={color} />;
+          } else if (route.name === 'Payment') {
+            iconName = 'dollar-sign';
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          } else if (route.name === 'Cart') {
+            iconName = 'shopping-cart';
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          } else if (route.name === 'Profile') {
+            iconName = 'user';
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          }
+        },
+        headerShown: false,
+        tabBarActiveTintColor: '#ff9800', // 활성화 아이콘 색상(orange)
+        tabBarInactiveTintColor: '#49454f', //아이콘 색상
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: '#f5f5f5', // 배경색상
+          borderTopWidth: 0, // 상단 경계선 없애기
+        }
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Payment" component={PaymentScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
---
+++ app/screens/PaymentScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Payment Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff', // 배경색 설정 (흰색)
+  },
+  text: {
+    color: '#000000', // 텍스트 색상 설정 (검은색)
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
