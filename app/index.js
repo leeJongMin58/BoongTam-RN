25dc2e0f132cf8e33b07726f41b761cfa620543b
ilchan123 <dlfcks9@gmail.com>
2024-12-24 11:47:59 +0900

    feat: 로그인 스크린 업데이트
    1. 로그인 화면 구현
    2. index.js에 경로 설정.

--- app/index.js
+++ app/index.js
@@ -1,30 +1,32 @@
-import { registerRootComponent } from 'expo'
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import {
-	useTypography,
-	TypographyProvider,
-} from '../src/utils/TypographyContext'
-import colors from '../src/styles/color'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import LoginScreen from './login/login/login'
+import { registerRootComponent } from 'expo';
+import { StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Colors from "../src/styles/color"
+
+// 로그인 관련 화면
+import FirstScreen from './login/FirstScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-	return (
-		<TypographyProvider>
-			<StatusBar style="auto" />
-			<LoginScreen />
-		</TypographyProvider>
-	)
+  return (
+      <Stack.Navigator initialRouteName="First">
+        <Stack.Screen
+          name="First"
+          component={FirstScreen}
+          options={{ title: '로그인 화면' }}
+        />
+      </Stack.Navigator>
+  );
 }
 
-registerRootComponent(App)
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: colors.orange200,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
+  container: {
+    flex: 1,
+    backgroundColor: Colors.orange200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
