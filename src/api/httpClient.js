761c901a474334577fe9bab8608e1ba8103a3bad
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-12-26 18:02:35 +0900

    FEAT : 토큰 필요 유무에 따른 Client 기능 추가

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,6 +1,27 @@
+// 임시로 URL 두겠습니다. 서버가 aws랑 연결하면 그때 .env파일 이용해서 BASE_URL 관리하겠습니다.
 const BASE_URL = 'http://192.168.162.10:3000/'
 
+// 토큰이 필요없는 통신
 class Client {
+	async get(endpoint, requestBody) {
+		return await fetchData(false, 'GET', endpoint, requestBody)
+	}
+
+	async put(endpoint, requestBody) {
+		return await fetchData(false, 'PUT', endpoint, requestBody)
+	}
+
+	async patch(endpoint, requestBody) {
+		return await fetchData(false, 'PATCH', endpoint, requestBody)
+	}
+
+	async delete(endpoint, requestBody) {
+		return await fetchData(false, 'DELETE', endpoint, requestBody)
+	}
+}
+
+// 토큰이 필요한 통신 Client With Token
+class ClientWT {
 	async get(endpoint, requestBody) {
 		return await fetchData('GET', endpoint, requestBody)
 	}
@@ -18,7 +39,7 @@ class Client {
 	}
 }
 
-async function fetchData(method, endpoint, requestBody) {
+async function fetchData(requireToken, method, endpoint, requestBody) {
 	try {
 		const options = {
 			method: method,
@@ -27,10 +48,20 @@ async function fetchData(method, endpoint, requestBody) {
 			},
 		}
 
+		// 토큰 생성 코드 완성되면 테스트 필요
+		// if (requireToken) {
+		// 	const token =
+		// 		localStorage.getItem('token') || sessionStorage.getItem('token')
+		// 	if (token) {
+		// 		options.headers['Authorization'] = `Bearer ${token}`
+		// 	} else {
+		// 		throw new Error('No token found')
+		// 	}
+		// }
+
 		if (requestBody) {
 			options.body = JSON.stringify(requestBody)
 		}
-
 		const resp = await fetch(BASE_URL + endpoint, options)
 
 		if (!resp.ok) {
