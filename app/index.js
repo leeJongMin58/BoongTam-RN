1ce4c85d712395a8ea6f891c181e3d952fc8110a
ilchan123 <dlfcks9@gmail.com>
2024-12-27 14:13:58 +0900

    fix: 경로수정 마이페이지에서 로그인페이지로.

--- app/index.js
+++ app/index.js
@@ -2,13 +2,12 @@ import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar'
 import { TypographyProvider } from '../src/utils/TypographyContext'
 import LoginScreen from './login/login/login'
-import MyScreen from './(tabs)/(my)/(main)/my'
 
 export default function App() {
 	return (
 		<TypographyProvider>
 			<StatusBar style="auto" />
-			<MyScreen />
+			<LoginScreen />
 		</TypographyProvider>
 	)
 }
