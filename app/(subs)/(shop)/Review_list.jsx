23f416a0219af2db07bf259fe26427a693c4d0fb
ilchan123 <dlfcks9@gmail.com>
2024-12-31 10:43:44 +0900

    feat: 상품에 대한 더미데이터
    1. 상품 내역 추가
    2. 리뷰 내역 추가

---
+++ app/(subs)/(shop)/Review_list.jsx
@@ -0,0 +1,13 @@
+const reviews = [
+    { id: '1', user: '붕어사랑님', comment: '정말 귀여운 상품이에요!', rating: 5 },
+    { id: '2', user: '싸만코님', comment: '배송이 빠르고 품질도 좋아요.', rating: 4 },
+    { id: '3', user: '붕탐고지님', comment: '가격 대비 훌륭한 제품입니다.', rating: 5 },
+    { id: '4', user: '슈븅은진리님', comment: '추천추천!', rating: 5 },
+    { id: '5', user: '놉팥붕님', comment: '나름 좋아요.', rating: 4 },
+    { id: '6', user: '판교가격이진리님', comment: '훌륭한 제품입니다.', rating: 5 },
+    { id: '7', user: '1000원하나님', comment: '가성비 값 상품이에요!', rating: 5 },
+    { id: '8', user: '이게맞나님', comment: '붕어가 빠르고 크림이 좋아요.', rating: 4 },
+    { id: '9', user: '슈크림빵님', comment: '나름 훌륭한 제품입니다.', rating: 5 },
+  ];
+
+  export default reviews;
