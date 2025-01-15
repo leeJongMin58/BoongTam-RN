717be444e49fb2491e6ac904cc23d764fec639b5
박치호 <qkrclgh2@gmail.com>
2025-01-15 12:59:12 +0900

    feat: 커뮤니티 부분 api연결 세팅

---
+++ src/usecases/communityUsecase.js
@@ -0,0 +1,29 @@
+import * as repository from '../repositories/communityRepository.js'
+
+// 매장 제보하기
+export const submitStore = async (storeInfo) => 
+    (await repository.submitStore(storeInfo))
+  
+// 매장 리뷰 작성
+export const writeStoreReview = async (store_id, review_text, review_rating, review_photos) => 
+    (await repository.writeStoreReview(store_id, review_text, review_rating, review_photos))
+
+// 굿즈 리뷰 작성
+export const writeGoodsReview = async (goods_id, review_text, review_photos) => 
+    (await repository.writeGoodsReview(goods_id, review_text, review_photos))
+
+// 매장 리뷰 가져오기
+export const fetchStoreReviews = async (authorization) => 
+    await repository.fetchStoreReviews(authorization)
+
+// 매장 인기리뷰 가져오기
+export const fetchPopularStoreReviews = async (authorization) => 
+    await repository.fetchPopularStoreReviews(authorization)
+
+// 굿즈 리뷰 가져오기
+export const fetchGoodsReviews = async (authorization) => 
+    await repository.fetchGoodsReviews(authorization)
+
+// 리뷰 좋아요 토글
+export const reviewLikeToggle = async (authorization) => 
+    await repository.reviewLikeToggle(authorization)
